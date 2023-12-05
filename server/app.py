d9b69ffd2fd6b431111d3c132fd2a801398723fc
Angelo Gentile III <angelo.john.gentile@gmail.com>
2023-12-04 20:45:12 -0500

    completed lab

--- server/app.py
+++ server/app.py
@@ -14,13 +14,41 @@ app.json.compact = False
 migrate = Migrate(app, db)
 db.init_app(app)
 
-
 @app.route('/')
 def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def earth_by_id(id):
+    earthquake = Earthquake.query.filter(Earthquake.id == id).first()
+
+    if earthquake:
+        body = earthquake.to_dict()
+        status = 200
+    else:
+        body = {
+            "message": f"Earthquake {id} not found."
+        }
+        status = 404
+
+    return make_response(body, status)
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def minimum_magnitude(magnitude):
+    earthquakes = []
+
+    for eq in Earthquake.query.filter(Earthquake.magnitude >= magnitude):
+        earthquakes.append(eq.to_dict())
+
+    body = {
+        'count': len(earthquakes),
+        'quakes': earthquakes
+    }
+
+    return make_response(body, 200)
+
 
 
 if __name__ == '__main__':
